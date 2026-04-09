9c941f7cecb99830347d9b174b64bb475868d541
Guillermo Chilavert Rauschmair <guillerausch614@gmail.com>
2026-04-08 22:53:59 -0300

    agregamos carrusel de destacados en principal

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TechCase</title>
     <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="{{ asset('vendor/css/bootstrap.min.css') }}"> 
 
 </head>
 <body>
@@ -16,7 +17,32 @@
         <a href="/sobre-nosotros">Sobre Nosotros</a>
     </nav>
     </div>
-    <h2>PRODUCTOS: </h2>
-    
+    <div class="container-md">
+        <h2>Tendencias: </h2>
+        <div id="carouselExampleFade" class="carousel slide carousel-fade">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="{{ asset('img/ej trendy 1.jpg') }}" class="d-block w-100" alt="..." style= "max-height: 350px">
+    </div>
+    <div class="carousel-item">
+      <img src="{{ asset('img/ej trendy 2.jpg') }}" class="d-block w-100" alt="..." style= "max-height: 350px">
+    </div>
+    <div class="carousel-item">
+      <img src="{{ asset('img/ej trendy 3.jpg') }}" class="d-block w-100" alt="..." style= "max-height: 350px">
+    </div>
+    <div class="carousel-item">
+      <img src="{{ asset('img/ej trendy 4.jpg') }}" class="d-block w-100" alt="..." style= "max-height: 350px">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+    </div>
+<script src="{{ asset('vendor/js/bootstrap.bundle.min.js') }}"> </script> 
 </body>
 </html> 
