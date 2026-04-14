9937061da9535089be39cad04cfd3b0266072db0
Mati-Degregorio <matidegre24@gmail.com>
2026-04-14 11:15:56 -0300

    agregamos la ubicacion al fouter

--- resources/views/parciales/pie-pagina.blade.php
+++ resources/views/parciales/pie-pagina.blade.php
@@ -2,14 +2,14 @@
   <div class="container py-4">
     <div class="row">
 
-      <div class="col-md-4">
+      <div class="col-md-3">
         <h4>¿Necesitás ayuda?</h4>
         <h5>contáctanos vía Mail</h5>
         <p class="mb-1">guillerausch614@gmail.com</p>
-        <p>contacto@techcase.com</p>
+        <p>matidegre24@gmail.com</p>
       </div>
 
-      <div class="col-md-4">
+      <div class="col-md-3">
         <h4>Enlaces de utilidad</h4>
         <ul class="list-unstyled">
           <li> <a href="#" class="text-white text-decoration-none">Sobre Nosotros</a></li>
@@ -17,11 +17,14 @@
         </ul>
       </div>
 
-      <div class="col-md-4">
+      <div class="col-md-2">
         <h5>Legal</h5>
         <p>© 2026 TechCase</p>
       </div>
-
+      <div class= "col-md-4">
+      <h4>ubicacion</h4>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3540.0826933073445!2d-58.83488852560505!3d-27.466684816575725!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94456ca6d11ee93d%3A0x597626256826f00a!2s9%20de%20Julio%201449%2C%20W3400%20Corrientes!5e0!3m2!1ses!2sar!4v1776174299815!5m2!1ses!2sar" width="300" height="150" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
     </div>
   </div>
 </footer>
