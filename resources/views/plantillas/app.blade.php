0497fdd8e1a79de63767232649a8fa175aadac67
Guillermo Chilavert Rauschmair <guillerausch614@gmail.com>
2026-04-26 14:58:54 -0300

    mejore el estilo del detalle de funda y de la barra de navegación

--- resources/views/plantillas/app.blade.php
+++ resources/views/plantillas/app.blade.php
@@ -6,6 +6,7 @@
     <title>TechCase</title>
     <link href="{{ asset('vendor/css/bootstrap.min.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 <body>
     @include('parciales.cabecera')
