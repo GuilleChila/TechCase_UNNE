807c98d65b48a8d825580d0c86c0fde0efe1618b
Guillermo Chilavert Rauschmair <guillerausch614@gmail.com>
2026-04-26 22:05:02 -0300

    navbar modificado y responsivo, quité banner de pasos en la pagina principal

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -6,64 +6,6 @@
       <p>En nuestra tienda, transformamos tu iPhone en un reflejo de tu personalidad. Te ofrecemos una selección exclusiva de fundas, cargadores y comecables diseñados no solo para proteger y potenciar tu dispositivo, sino para que cada detalle hable de ti. Dale a tu teléfono ese toque único y personalízalo exactamente a tu gusto con nuestros accesorios.</p>
   </div>
 </section>
-<section class= "container">
-  <div id="carouselExampleIndicators" class="carousel slide">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="{{ asset('img/paso1 techcase.png') }}" class="d-block w-100" alt="..." style= "max-height: 300px">
-    </div>
-    <div class="carousel-item">
-      <img src="{{ asset('img/paso2 techcase.png') }}" class="d-block w-100" alt="..." style= "max-height: 300px">
-    </div>
-    <div class="carousel-item">
-      <img src="{{ asset('img/paso3 techcase.png') }}" class="d-block w-100" alt="..." style= "max-height: 300px">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-</section>
-
-<section class="instagram-section container-sm">
-  <div class="insta-header">
-    <h2>¡Siguenos en Instagram!</h2>
-    <p class="insta-subtitle">Para ver nuestras publicaciones destacadas</p>
-    </div>
-    <div class="insta-user">
-      <i class="fab fa-instagram"></i>
-       <a href="https://www.instagram.com/tech.case__?igsh=ZG1iNTlva2M0ZGFv" target="_blank" class="seguinos-link"><strong>TechCase__</strong></a>
-  </div>
-
-  <div class="insta-grid">
-    <a href="https://www.instagram.com/p/DIFEBqWPL5l/?hl=es-la" target="_blank" class="insta-item">
-      <img src="{{ asset('img/postIG1.png') }}" alt="Funda para teléfono">
-    </a>
-    
-    <a href="https://www.instagram.com/p/DIFEGauPQJ1/?hl=es-la" target="_blank" class="insta-item">
-      <img src="{{ asset('img/postIG2.png') }}" alt="Funda para teléfono">
-    </a>
-
-    <a href="https://www.instagram.com/p/DIFEITSPfdt/?hl=es-la" target="_blank" class="insta-item">
-      <img src="{{ asset('img/postIG3.png') }}" alt="Funda para teléfono">
-    </a>
-
-    <a href="https://www.instagram.com/p/DJqFeVcuFXZ/?hl=es-la" target="_blank" class="insta-item">
-      <img src="{{ asset('img/postIG4.png') }}" alt="Funda para teléfono">
-    </a>
-
-    </div>
-</section>
 
     <div class="container-md">
         <h2>Tendencias: </h2>
@@ -132,6 +74,36 @@
         </div>
         </a>
       </div>
+    </div>
+</section>
+
+<section class="instagram-section container-sm">
+  <div class="insta-header">
+    <h2>¡Siguenos en Instagram!</h2>
+    <p class="insta-subtitle">Para ver nuestras publicaciones destacadas</p>
+    </div>
+    <div class="insta-user">
+      <i class="fab fa-instagram"></i>
+       <a href="https://www.instagram.com/tech.case__?igsh=ZG1iNTlva2M0ZGFv" target="_blank" class="seguinos-link"><strong>TechCase__</strong></a>
+  </div>
+
+  <div class="insta-grid">
+    <a href="https://www.instagram.com/p/DIFEBqWPL5l/?hl=es-la" target="_blank" class="insta-item">
+      <img src="{{ asset('img/postIG1.png') }}" alt="Funda para teléfono">
+    </a>
+    
+    <a href="https://www.instagram.com/p/DIFEGauPQJ1/?hl=es-la" target="_blank" class="insta-item">
+      <img src="{{ asset('img/postIG2.png') }}" alt="Funda para teléfono">
+    </a>
+
+    <a href="https://www.instagram.com/p/DIFEITSPfdt/?hl=es-la" target="_blank" class="insta-item">
+      <img src="{{ asset('img/postIG3.png') }}" alt="Funda para teléfono">
+    </a>
+
+    <a href="https://www.instagram.com/p/DJqFeVcuFXZ/?hl=es-la" target="_blank" class="insta-item">
+      <img src="{{ asset('img/postIG4.png') }}" alt="Funda para teléfono">
+    </a>
+
     </div>
 </section>
 @endsection
